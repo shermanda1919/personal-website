db2bd2fbd92753f66d0bb71357354d8750f785f2
Amanda Sherman <you@example.com>
2024-10-14 23:09:01 -0400

    push changes to github

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
+import Footer from "./Footer";
+import Typewriter from 'react-typewriter-effect';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Personal Website</h1>
-        <p className="text-gray-600 mb-6">This is a brief introduction about me.</p>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-          Get Started
-        </button>
+        <h1 className="text-4xl font-bold mb-4">Hello, I'm Amanda Sherman</h1>
+        <p className="text-2xl italic text-gray-600 mb-6">CS Major @ UMass Amherst</p>
+        <img src="" className="w-32 h-auto mx-auto" />
       </div>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mt-6 mb-6">My Experience</h1>
+        <div className="ml-2">
+          <p className="text-left text-xl">Research Assistant @UMass Amherst (Oct 2024 - Present)</p>
+          <ul className="list-disc text-left mb-4">
+            <li className="text-gray-600">Conducting research on NLP (Natural Language Processing), specifically simplifying text outputs from prompts, under PhD student Maha Alkhairy</li>
+          </ul>
+          <p className="text-left text-xl">Founder @FuseBloom (Jun 2020 - Present)</p>
+          <ul className="text-left list-disc text-left mb-4">
+            <li className="text-gray-600">Founder of FuseBloom, a free online platform for high schoolers that custom curates internship and summer opportunities to advance educational equality</li>
+            <li className="text-gray-600">1000+ high schoolers subscribed to our opportunities newsletter</li>
+            <li className="text-gray-600">800+ followers on Instagram</li>
+            <li className="text-gray-600">2nd Place Winner of 2023 Minute Pitch entrepreneurship competition</li>
+            <li className="text-gray-600">60% Acceptance Rate of high school students into internships (12 out of 20 students total)</li>
+          </ul>
+          <p className="text-left text-xl">Research Assistant @UMass Amherst (Jun 2024 - Aug 2024)</p>
+          <ul className="list-disc text-left mb-4">
+            <li className="text-gray-600">Accepted into URV (Undergraduate Research Volunteers) program to perform collaborative research study on Membership Inference Attacks in Federated Learning with two teammates under PhD mentor Dayeon Kang</li>
+            <li className="text-gray-600">Graphed and analyzed benchmark ML datasets (CIFAR-10, CIFAR-100, MNIST, FMNIST, etc.) using Python and Tensorflow in Matplotlib</li>
+            <li className="text-gray-600">Spearheaded research presentation to fellow undergraduate and graduate students at First Friday Fair</li>
+          </ul>
+          <p className="text-left text-xl">Fellow @Valley Venture Mentors (Jun 2024 - Aug 2024)</p>
+          <ul className="list-disc text-left mb-4">
+            <li className="text-gray-600">Learned about process of launching successful startup</li>
+            <li className="text-gray-600">Networked and collaborated with Pioneer Valley entrepreneurs to refine FuseBloom's business model</li>
+          </ul>
+          <p className="text-left text-xl">Director of Outreach @Hack3 (Jul 2020 - Jun 2022)</p>
+          <ul className="list-disc text-left mb-4">
+            <li className="text-gray-600">Coordinate all outreach and social media operations for Hack3</li>
+            <li className="text-gray-600">Design, lay out, and post content on our Instagram feed</li>
+            <li className="text-gray-600">Gained 275+ participants during 2020 hackathon</li>
+            <li className="text-gray-600">Gained nearly 350 participants during 2021 hackathon (largest high school hackathon of 2021!)</li>
+            <li className="text-gray-600">Gained 377 participants during 2022 hackathon (largest high school hackathon of 2022 so far!)</li>
+            <li className="text-gray-600">2022 MLH Hackathon</li>
+          </ul>
+        </div>
+        
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    const year = new Date().getFullYear();
+
+    return <footer>{`Copyright © Amanda Sherman ${year}`}</footer>;
+};
+
+export default Footer;
